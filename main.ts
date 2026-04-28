376de9168aea7f32f13463c1f412c81fb2b6464e
nmzxzondc <criesalotperiod@gmail.com>
2026-04-28 16:11:51 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -7,10 +7,20 @@ let total_cup_capcity_ml = 798
 let time_to_fill_cup = 10
 let time_to_fill_1ml = (time_to_fill_cup / total_cup_capcity_ml)
 
-if (!SuperBitV2.MotorRun) {
+// if you change the speed, record the time it takes to fill a cup up again
+/*
+
+error codes: 
+ml: the mililiters_to_draw is below 0 or greater than the cup's total capacity
+lib: the SuperBitV2 library was either not found or SuperBitV2.MotorRun was not found
+db: the pump is already running
+hi: the pump speed exceeds 255 or is less then -255.
+ */
+
+if (!SuperBitV2.enMotors.M1) {
     basic.showString("lib")
-    basic.forever(function() {
-      basic.pause(5 * 1000)  
+    basic.forever(function () {
+        basic.pause(5 * 1000)
     })
 } else {
     basic.showString("P")
@@ -19,10 +29,14 @@ if (!SuperBitV2.MotorRun) {
 function RunPump(duration: number, pumpspeed: number) {
     if (pump_running) {
         basic.showString("db")
+
+        basic.pause(2 * 1000)
+
+        basic.showString("P")
         return
     }
 
-    if (pumpspeed > 255) {
+    if (pumpspeed > 255 || pumpspeed < -255) {
         basic.showString("hi")
         return
     }
