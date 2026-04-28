76de59e3c05534bfe9b1303cc14e6d3120e0fd0e
nmzxzondc <criesalotperiod@gmail.com>
2026-04-28 18:13:22 -0400

    Fixed debounce not working

--- main.ts
+++ main.ts
@@ -41,47 +41,31 @@ function RunCheck() {
 
 basic.showString("O")
 
-basic.forever(RunCheck)
-
-
+//basic.forever(RunCheck)
 
 function RunPump(duration: number, pump_speed: number, debounce: boolean) {
-    if (debounce) {
-        basic.showString("db")
-        return
-    }
-
-    pump_running = true
-    basic.showString("P")
-
     SuperBitV2.MotorRun(SuperBitV2.enMotors.M1, pump_speed)
-    basic.pause(10 * 1000)
+    basic.pause(5 * 1000)
     SuperBitV2.MotorStopAll
 
     basic.showString("O")
 }
 
-function Drawmilliliters(ml: number) {
-
-    if (pump_running) {
-        basic.clearScreen()
-        basic.showString("A")
-        return
-    }
-    
-
+function DrawMilliliters(ml: number) {
     let time_to_fill = time_to_fill_1ml * ml
     RunPump(time_to_fill, speed, pump_running)
 
     basic.clearScreen()
     basic.showString("FINI")
-    pump_running = false
+
 }
 
-input.onButtonPressed(Button.A, function () {
-    
-    
-    Drawmilliliters(milliliters_to_draw)
-
-    
+basic.forever(function() {
+    if (!pump_running && input.buttonIsPressed(Button.A)) {
+        basic.showString("P")
+        pump_running = true
+        DrawMilliliters(milliliters_to_draw)
+        pump_running = false
+    }
 })
+    
